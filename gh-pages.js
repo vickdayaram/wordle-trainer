dc998452617fb757270862bb1fde3b418d0d0140
Pradeep Dayaram <vickdayaram@gmail.com>
2022-08-11 20:12:29 -0400

    update github pages

--- gh-pages.js
+++ gh-pages.js
@@ -4,7 +4,7 @@ ghpages.publish(
     'public', // path to public directory
     {
         branch: 'gh-pages',
-        repo: 'https://github.com/username/yourproject.git', // Update to point to your repository  
+        repo: 'https://github.com/vddayara/wordle-trainer.git', // Update to point to your repository  
         user: {
             name: 'Pradeep Dayaram', // update to use your name
             email: 'vickdayaram@gmail.com' // Update to use your email
